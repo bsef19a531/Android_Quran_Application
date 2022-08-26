f6ddabccf952177aa571c9a7d363933cb9ee9295
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-08-27 00:13:47 +0500

    Added Main Logic

--- app/src/main/java/com/example/quranmajeed/MainActivity.java
+++ app/src/main/java/com/example/quranmajeed/MainActivity.java
@@ -1,14 +1,48 @@
-package com.example.quranmajeed;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.quranmajeed.CustomAdapter;
+import com.example.quranmajeed.QDH;
+import com.example.quranmajeed.QuranArabicText;
+import com.example.quranmajeed.R;
+import com.example.quranmajeed.SurahActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lst = findViewById(R.id.main_list);
+        QDH surahNames = new QDH();
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,  surahNames.urduSurahNames);
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.custom_list,  surahNames.urduSurahNames);
+        lst.setAdapter(adapter);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int index = surahNames.SSP[i];
+                int end = surahNames.SSP[i+1];
+                QuranArabicText temp = new QuranArabicText();
+                if (index == 6342) {
+                    end = temp.QuranArabicText.length - 1;
+                }
+                Intent intent = new Intent(MainActivity.this, SurahActivity.class);
+
+                intent.putExtra("index", index);
+                intent.putExtra("end", end);
+                startActivity(intent);
+            }
+        });
     }
 }
